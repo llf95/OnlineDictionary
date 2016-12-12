7fff4bd451cd6b38b94dbc71cd39b13d0a9ea593
MrJxy <jiangxy1001@outlook.com>
2016-12-12 20:50:14 +0800

    Add some jars in .\lib;
    *Add javax.servlet.jsp.jstl.jar;
    *Add javax.servlet.jsp.jstl-api.jar;
    *Add mysql-connector-java-5.1.40-bin.jar

--- src/RequestPackage.java
+++ src/RequestPackage.java
@@ -9,15 +9,12 @@ public class RequestPackage implements Serializable {
     public void setType(String type) {
         this.type = type;
     }
-
     public void addRequest(String req) {
         this.content.add(req);
     }
-
     public String getType() {
         return this.type;
     }
-
     public Vector<String> getRequest() {
         return this.content;
     }
